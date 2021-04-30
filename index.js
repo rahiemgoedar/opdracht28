f6eb2ac853b29ed9646e769011739f7dfd3b9a6d
rahiemgoedar <79698303+rahiemgoedar@users.noreply.github.com>
2021-04-30 14:21:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// voorbeeld while loop:
+var age = 17;
+
+while (age < 40) {
+    console.log("your too young!");
+    age++;
+}
+document.write("your 40 now, welcome!")
+
+//voorbeeld for:
+for (age = 17; age < 40; age++) {
+    console.log("you're too young!");
+}
+document.write("welcome to the 40's club!");
+
+
+// Even Odd Reporter
+
+/* for (index = 1; index < 100; index*index) {
+    console.log(index);
+} */
+
